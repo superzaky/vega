995a83399f79118d1cbbc2314c9c641f59f82785
superzaky <superkytoz@gmail.com>
2017-05-23 09:53:09 +0200

    Apply constraints.

--- Migrations/20170523074409_ApplyConstraints.Designer.cs
+++ Migrations/20170523074409_ApplyConstraints.Designer.cs
@@ -8,9 +8,10 @@ using Vega.Persistence;
 namespace Vega.Migrations
 {
     [DbContext(typeof(VegaDbContext))]
-    partial class VegaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170523074409_ApplyConstraints")]
+    partial class ApplyConstraints
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")

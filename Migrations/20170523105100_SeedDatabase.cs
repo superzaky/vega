b13047d663acab5ff99462e993f3491644252a2a
superzaky <superkytoz@gmail.com>
2017-05-23 15:32:31 +0200

    Seed the database.

---
+++ Migrations/20170523105100_SeedDatabase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make1')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make2')");
+            migrationBuilder.Sql("INSERT INTO Makes (Name) VALUES ('Make3')");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModelA', (SELECT ID FROM Makes WHERE Name = 'Make1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModelB', (SELECT ID FROM Makes WHERE Name = 'Make1'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make1-ModelC', (SELECT ID FROM Makes WHERE Name = 'Make1'))");
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make2-ModelA', (SELECT ID FROM Makes WHERE Name = 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make2-ModelB', (SELECT ID FROM Makes WHERE Name = 'Make2'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make2-ModelC', (SELECT ID FROM Makes WHERE Name = 'Make2'))");
+
+
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make3-ModelA', (SELECT ID FROM Makes WHERE Name = 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make3-ModelB', (SELECT ID FROM Makes WHERE Name = 'Make3'))");
+            migrationBuilder.Sql("INSERT INTO Models (Name, MakeID) VALUES ('Make3-ModelC', (SELECT ID FROM Makes WHERE Name = 'Make3'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            /*We don't need to delete the Models, because when we delete the Makes all associated Models would also be deleted.*/
+            migrationBuilder.Sql("DELETE FROM Makes WHERE Name In ('Make1', 'Make2', 'Make3')");
+        }
+    }
+}

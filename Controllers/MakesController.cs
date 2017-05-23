145f7a2de6d9d24ac9aa20ad9a7513ba7a9be8de
superzaky <superkytoz@gmail.com>
2017-05-23 16:46:28 +0200

    Build an API.

---
+++ Controllers/MakesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vega.Models;
+using Vega.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Vega.Controllers
+{
+    public class MakesController : Controller
+    {
+        private readonly VegaDbContext context;
+
+        public MakesController(VegaDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/api/makes")]
+        public async Task <IEnumerable<Make>> GetMakes()
+        {
+            return await context.Makes.Include(m => m.Models).ToListAsync();
+        }
+    }
+}

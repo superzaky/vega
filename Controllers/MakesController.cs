5fb7d18a6fe30efb8bbd37d62c2405688af8e23c
superzaky <superkytoz@gmail.com>
2017-05-24 14:09:12 +0200

    Add AutoMapper.

--- Controllers/MakesController.cs
+++ Controllers/MakesController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Vega.Models;
 using Vega.Persistence;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using Vega.Controllers.Resources;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,9 +16,11 @@ namespace Vega.Controllers
     public class MakesController : Controller
     {
         private readonly VegaDbContext context;
+        private readonly IMapper mapper;
 
-        public MakesController(VegaDbContext context)
+        public MakesController(VegaDbContext context, IMapper mapper)
         {
+            this.mapper = mapper;
             this.context = context;
         }
 
@@ -27,9 +31,11 @@ namespace Vega.Controllers
         }
 
         [HttpGet("/api/makes")]
-        public async Task <IEnumerable<Make>> GetMakes()
+        public async Task <IEnumerable<MakeResource>> GetMakes()
         {
-            return await context.Makes.Include(m => m.Models).ToListAsync();
+            var makes = await context.Makes.Include(m => m.Models).ToListAsync();
+
+            return mapper.Map<List<Make>, List<MakeResource>>(makes);
         }
     }
 }
---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vega.Controllers.Resources;
+using Vega.Models;
+
+namespace Vega.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            //Unidirectional maps and currently we can only map Make to MakeResource and not the other way around. For that we need to create a seperate map.
+            CreateMap<Make, MakeResource>();
+            CreateMap<Model, ModelResource>();
+        }
+    }
+}

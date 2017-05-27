bcc3013eeeec13dfde210dc4b128747d12009d4c
superzaky <superkytoz@gmail.com>
2017-05-27 20:12:22 +0200

    Add vehicle.

---
+++ Models/Vehicle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vega.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        /*The reason we have the two properties below here is that it's easier to persist objects using the foreign key property. So in the future
+         when we want to create a vehicle or when we want to update a vehicle. We don't have to load a Model object from the context in order to
+         associate it with this vehicle. We can simply set the identifier of the model using the ModelId property. */
+        //foreign key property
+        public int ModelId { get; set; }
+        //navigation property
+        public Model Model { get; set; }
+        public bool IsRegistered { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string ContactName { get; set; }
+
+        [StringLength(255)]
+        public string ContactEmail { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContactPhone { get; set; }
+        public DateTime LastUpdate { get; set; }
+    }
+}

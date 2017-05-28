d0644014527d4c41edc8cae4da7e9ac966f2ba5c
superzaky <superkytoz@gmail.com>
2017-05-28 21:45:50 +0200

    Implement mapping between VehicleResource and Vehicle.

---
+++ Controllers/VehiclesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Vega.Controllers.Resources;
+using AutoMapper;
+using Vega.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Vega.Controllers
+{
+    [Route("api/vehicles")]
+    public class VehiclesController : Controller
+    {
+        private readonly IMapper mapper;
+        public VehiclesController(IMapper mapper)
+        {
+            this.mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult CreateVehicle([FromBody] VehicleResource vehicleResource)
+        {
+            var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+            return Ok(vehicle);
+        }
+    }
+}
--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -12,10 +12,20 @@ namespace Vega.Mapping
     {
         public MappingProfile()
         {
+            /*So far all the mappings we have are from Domain to API Resources */
             //Unidirectional maps and currently we can only map Make to MakeResource and not the other way around. For that we need to create a seperate map.
             CreateMap<Make, MakeResource>();
             CreateMap<Model, ModelResource>();
             CreateMap<Feature, FeatureResource>();
+
+            //Below we have API Resource to Domain
+            CreateMap<VehicleResource, Vehicle>()
+                .ForMember(v => v.ContactName, opt => opt.MapFrom(vr => vr.Contact.Name))
+                .ForMember(v => v.ContactEmail, opt => opt.MapFrom(vr => vr.Contact.Email))
+                .ForMember(v => v.ContactPhone, opt => opt.MapFrom(vr => vr.Contact.Phone))
+                /*new VehicleFeature { FeatureId = id } <-- Dat is volgens mij C# style object initialization. Let op dat dit niet hetzelfde is als het aanmaken van een object en argumenten
+                 aan het mee geven bent aan de constructor*/
+                .ForMember(v => v.Features, opt => opt.MapFrom(vr => vr.Features.Select(id => new VehicleFeature { FeatureId = id })));
         }
     }
 }

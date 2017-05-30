b031f4e2c6b272ef00838b23b230db639c43bb2f
superzaky <superkytoz@gmail.com>
2017-05-30 13:28:30 +0200

    Build an API to delete a vehicle.

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -61,6 +61,10 @@ namespace Vega.Controllers
                 return BadRequest(ModelState);
 
             var vehicle = await context.Vehicles.Include( v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+            if (vehicle == null)
+                return NotFound();
+
             mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
             vehicle.LastUpdate = DateTime.Now;
 
@@ -69,6 +73,20 @@ namespace Vega.Controllers
             var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
 
             return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVehicle(int id)
+        {
+            var vehicle = await context.Vehicles.FindAsync(id);
+
+            if (vehicle == null)
+                return NotFound();
+
+            context.Remove(vehicle);
+            await context.SaveChangesAsync();
+
+            return Ok(id);
         }
     }
 }

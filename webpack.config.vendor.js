961acb45f242a1fad32d157c4ba2bc5d0391d4df
superzaky <superkytoz@gmail.com>
2017-06-23 20:59:01 +0200

    Create interfaces.

--- webpack.config.vendor.js
+++ webpack.config.vendor.js
@@ -34,6 +34,7 @@ module.exports = (env) => {
                 'ng2-toasty/bundles/style-bootstrap.css',
                 'jquery',
                 'raven-js',
+                'underscore',
                 'zone.js',
             ]
         },

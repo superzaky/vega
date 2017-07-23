28b806835cd971f3333b2d4d41cd1010634da0ee
superzaky <superkytoz@gmail.com>
2017-07-23 17:25:46 +0200

    Add bootstrap progress bar.

--- ClientApp/app/components/view-vehicle/view-vehicle.ts
+++ ClientApp/app/components/view-vehicle/view-vehicle.ts
@@ -1,6 +1,6 @@
 ﻿import { ToastyService } from 'ng2-toasty';
 import { VehicleService } from './../../services/vehicle.service';
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, NgZone } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PhotoService } from "../../services/photo.service";
 import { ProgressService } from "../../services/progress.service";
@@ -15,8 +15,10 @@ export class ViewVehicleComponent implements OnInit {
     vehicle: any;
     vehicleId: number;
     photos: any[];
+    progress: any;
 
     constructor(
+        private zone: NgZone,
         private route: ActivatedRoute,
         private router: Router,
         private toasty: ToastyService,
@@ -62,7 +64,18 @@ export class ViewVehicleComponent implements OnInit {
 
         /*Before uploading the file to the server we want to subscribe to our ProgressService*/
         this.progressService.uploadProgress
-            .subscribe(progress => console.log(progress));
+            .subscribe(progress => {
+                console.log(progress);
+                // Set the progress to this.progress first
+                // before Angular has a chance to monkey patch
+                this.zone.run(() => {
+                    this.progress = progress;
+                });
+            },
+            //set the progress field to null
+            null,
+            //for the completion of the observable
+            () => { this.progress = null });
 
         this.photoService.upload(this.vehicleId, nativeElement.files[0])
             .subscribe(photo => {

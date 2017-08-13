d246addb3b4b771598e858b8e0c79f9bbc20dda7
superzaky <superkytoz@gmail.com>
2017-08-13 18:43:08 +0200

    Fix the issue with roles of new users being undefined.

--- ClientApp/app/services/auth.service.ts
+++ ClientApp/app/services/auth.service.ts
@@ -40,7 +40,7 @@ export class AuthService {
         if (token) {
             var jwtHelper = new JwtHelper();
             var decodedToken = jwtHelper.decodeToken(token);
-            this.roles = decodedToken['https://vega.com/roles'];
+            this.roles = decodedToken['https://vega.com/roles'] || [];
         }
     }
 
